692e4f397d5ebaae446d6682c50b91d53823e89d
Chai Ching Ting <joliangel288@gmail.com>
2017-05-07 04:28:05 +0800

    PhotoRecord

---
+++ app/src/main/java/com/example/vivi/homeworkdemo/AlbumItem.java
@@ -0,0 +1,16 @@
+package com.example.vivi.homeworkdemo;
+
+/**
+ * Created by vivi on 2017/5/7.
+ */
+
+public class AlbumItem {
+
+    AlbumItem(String name, int imgid) {
+        this.name = name;
+        this.imgid = imgid;
+    }
+
+    String name;
+    int imgid;
+}
